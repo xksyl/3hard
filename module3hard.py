e6934c2436049ae225833ca47567d94516859fd1
xksyl <egorka2004212@gmail.com>
2024-07-18 14:57:39 +0500

    Add files via upload

--- module3hard.py
+++ module3hard.py
@@ -1,1 +1,20 @@
 data_structure = [[1, 2, 3],{'a': 4, 'b': 5},(6, {'cube': 7, 'drum': 8}),"Hello",((), [{(2, 'Urban', ('Urban2', 35))}])]
+
+def calculate_structure_sum (ds):
+    result = []
+    for i in ds:
+        if isinstance(i, int):
+            result.append(i)
+        elif isinstance(i, str):
+            result.append(len(i))
+        elif isinstance(i, dict):
+            result.append(calculate_structure_sum(list(i.keys())))
+            result.append(calculate_structure_sum(list(i.values())))
+        else:
+            if len(i) > 0:
+                result.append(calculate_structure_sum(i))
+    else:
+        return sum(result)
+
+result = calculate_structure_sum(data_structure)
+print(result)
